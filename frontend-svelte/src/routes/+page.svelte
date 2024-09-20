a42c3dfff1a62a55912c5db7a84f8e853398c815
Andreas Bresser <self@andreasbresser.de>
2024-09-20 22:13:28 +0200

    add todos and basic model

--- frontend-svelte/src/routes/+page.svelte
+++ frontend-svelte/src/routes/+page.svelte
@@ -1,19 +1,53 @@
 <script lang="ts">
+  import IGame from '$lib/model/IGame';
   import Button, { Label } from '@smui/button';
-  import Card, { Content } from '@smui/card';
-  import Actions from '@smui/card/src/Actions.svelte';
-  import LayoutGrid from '@smui/layout-grid';
-  import Cell from '@smui/layout-grid/src/Cell.svelte';
+  import Card, { Content, Actions } from '@smui/card';
+  import LayoutGrid, { Cell } from '@smui/layout-grid';
 
-  const games = [{}, {}, {}, {}]
+  const games: IGame[] = [
+    {
+      name: "A game that has been played in the past"
+    },
+    {
+      name: 'game 2'
+    }
+  ]
+
+  // TODO: fetch data from previous runs
+  // TODO: websocket to get live data and show data on canvas (as well as state machine, ...)
 </script>
+
+Game streams:
 
 <LayoutGrid>
   {#each games as game}
   <Cell>
     <div class="card-container">
       <Card>
-        <Content>A game that has been played in the past.</Content>
+        <Content>{game.name}</Content>
+        <Actions>
+          <Button>
+            <Label>delete</Label>
+          </Button>
+          <Button>
+            <Label>view</Label>
+          </Button>
+        </Actions>
+      </Card>
+    </div>
+  </Cell>
+  {/each}
+</LayoutGrid>
+
+Game recordings:
+
+
+<LayoutGrid>
+  {#each games as game}
+  <Cell>
+    <div class="card-container">
+      <Card>
+        <Content>{game.name}</Content>
         <Actions>
           <Button>
             <Label>delete</Label>
